b48291bc5538e20c175091fd1c908297901f0672
yaokisan <yaoki.kobayashi@empire-art.jp>
2025-06-06 20:18:43 +0900

    Fix: Migrate to @supabase/ssr and add type definitions

--- src/utils/supabase-browser.ts
+++ src/utils/supabase-browser.ts
@@ -1,6 +1,9 @@
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-// 型生成ツールで生成した Database 型を使用する場合は下記の import を有効にしてください。
-// import { Database } from "@/types/supabase";
+import { createBrowserClient } from "@supabase/ssr";
+
+import { Database } from "../supabase";
 
 // 汎用的に any を使用していますが、Database 型を用意すると型安全になります。
-export const supabase = createBrowserSupabaseClient<any>(); 
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+); 
--- src/utils/supabase-server.ts
+++ src/utils/supabase-server.ts
@@ -1,5 +1,6 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../supabase";
 // 型生成ツールで生成した Database 型を使用する場合は下記の import を有効にしてください。
 // import { Database } from "@/types/supabase";
 
@@ -7,7 +8,21 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 export function createSupabaseServer() {
   const cookieStore = cookies();
   // `any` を Database 型に置き換えることで型安全になります。
-  return createServerComponentClient<any>({
-    cookies: () => cookieStore,
-  });
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
 } 
